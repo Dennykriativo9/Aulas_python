14a904c40ce0fb59bdb58b7f39e35df96c97c02f
Dennykriativo9 <147348390+Dennykriativo9@users.noreply.github.com>
2024-01-13 20:00:09 +0100

    Add files via upload

---
+++ teste_ping.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+os.system
+def pegar_ip():
+
+    valor =input("Introduz o iP desejado : ")
+    return valor
+
+#funçaõ principal
+
+
+def main():
+    ip = pegar_ip()
+    print(f" o ip introduzido foi {ip}")
+
+    os.system("cls")
+    os.system(f"ping {ip}")
+
+main()
+
+
+
