a6d6cc43cdf79e9d78ef9fdc87ff331e25108d4b
Denny Salgueiro <147348390+Dennykriativo9@users.noreply.github.com>
2024-07-01 12:22:32 +0100

    Add files via upload

---
+++ exercicio 2.py
@@ -0,0 +1,5 @@
+#  2) Faça um programa que leia um numero real e o imprima.
+
+#Resolução
+num_real = float(input("Digite um numero Real Qualquer : "))
+print(f" \n O numero REAL Digitado Foi {num_real} ")
